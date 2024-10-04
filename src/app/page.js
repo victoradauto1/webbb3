5889204b994adea6966171cf1531428a22d31385
Victor Adauto <victoradauto@yahoo.com.br>
2024-10-03 21:37:04 -0300

    - Configuração inicial o next/react; - Instalação do bootstrap; - Criação da tela de Login; - Criação dos estilos customizados custom-login-btn, custom-login-btn:hover, custom-rounded;

--- src/app/page.js
+++ src/app/page.js
@@ -1,94 +1,46 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import { useEffect } from "react";
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  useEffect(() => {
+    document.title = "Webbb3 | Login";
+  }, []);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  function btnLoginClick(){
+    alert("Ativando função de login!")
+  }
+
+  return (
+    <div className="container col-xxl-8 px-4 py-5">
+      <div className="row flex-lg-row-reverse aling-items-center g-5 py-5">
+        <div className="col-10 col-sm-8 col-lg-6">
+          <img
+            src="https://t2.tudocdn.net/653782?w=824&h=494"
+            className="d-block mx-lg-auto img-fluid custom-rounded"
+            width="700"
+            height="500"
+          />
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
+        <div className="col-lg-6">
+          <h1 className="display-5 fw-bold text-body-emphasis lh-1 mb-3">Weeeb3</h1>
+          <p className="lead">Votação on-chain do BBB.</p>
+          <p className="lead mb-3">Autentique-se com sua carteira e deixe seu voto para o próximo paredão.</p>
+          <div className="d-grid gap-2 d-md-flex justify-content-md-start">
+            <button type="button" onClick={btnLoginClick} className="custom-login-btn">
+              <img src="/images/MetaMask_Fox.svg.png" width="64" className="me-3" />
+              Conectar com a Metamask
+            </button>
+          </div> 
+        </div>
+      </div>
+      <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+        <p className="col-md-4 mb-0 text-body-secondary">&copy; 2024 Webbb3, Inc</p>
+        <ul className="nav col-md-4 justify-content-end">
+        <li className="nav-items"><a href="/" className="nav-item px-2 text-body-secondary text-decoration-none">Home</a></li>
+        <li className="nav-items"><a href="/about" className="nav-item px-2 text-body-secondary text-decoration-none">Quem somos</a></li>
+        </ul>
       </footer>
     </div>
   );
